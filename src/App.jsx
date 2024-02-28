caed0b408b60e24dfc2672cce4c9f142cbbd50f0
Suresh@9789977832 <sanddysuresh@gmail.com>
2024-02-28 18:13:27 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-black font-mono text-xl">
+      Hello world
+    </div>
+  )
+}
+
+export default App
